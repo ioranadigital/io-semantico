ed1674d61cbba5d00ce19b711f9c53d372d8f08f
ioranadigital <honatuya@gmail.com>
2026-06-18 08:13:53 +0200

    Revert "style: Pegar div.hero2.hero-planes al breadcrumb-container (todas las páginas)"
    This reverts commit 37fff5fe1b3f951a2eebd29e934b1e45ed2fdee3.

--- app/interno/ioranaseo/src/app/Components/HeroBanner/HeroBannerPlanes.tsx
+++ app/interno/ioranaseo/src/app/Components/HeroBanner/HeroBannerPlanes.tsx
@@ -39,7 +39,7 @@ const HeroBannerPlanes: FC<HeroBannerPlanesProps> = ({
           minHeight: "600px",
           margin: "0",
           padding: "0",
-          marginTop: "-50px",
+          marginTop: "-25px",
         }}
       >
         <div className="container">
